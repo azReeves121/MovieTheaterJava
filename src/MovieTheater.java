ea2c31fa1ab2f183e6bc1df7011bc249eb31bb24
azReeves121 <147558144+azReeves121@users.noreply.github.com>
2025-01-22 23:39:25 -0330

    constructor

--- src/MovieTheater.java
+++ src/MovieTheater.java
@@ -9,7 +9,12 @@ public class MovieTheater {
     private final char[][] seats;
 
 
-
+    public MovieTheater(int rows, int seatsPerRow) {
+        this.rows = rows;
+        this.seatsPerRow = seatsPerRow;
+        this.seats = new char[rows][seatsPerRow];
+        initializeSeats();
+    }
 
 
 
