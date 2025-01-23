6a5640b2e2b99054eef9d6529267dd37b3b5ab0c
azReeves121 <147558144+azReeves121@users.noreply.github.com>
2025-01-22 23:52:19 -0330

    displaySeating Row labels

--- src/MovieTheater.java
+++ src/MovieTheater.java
@@ -27,11 +27,36 @@ public class MovieTheater {
 
     public void displaySeating() {
         System.out.println("\nSeating Chart:");
-        System.out.print("   "); // Indentation for column numbers
+        System.out.print("   ");
         for (int i = 0; i < seatsPerRow; i++) {
             System.out.print((i + 1) + " ");
         }
         System.out.println();
+
+
+
+        for (int i = 0; i < rows; i++) {
+            System.out.print((char) ('A' + i) + "  ");
+            for (int j = 0; j < seatsPerRow; j++) {
+                System.out.print(seats[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
