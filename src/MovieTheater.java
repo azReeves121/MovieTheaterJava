2d59734df7edd373dcc7ad0f73334e72c3698601
azReeves121 <147558144+azReeves121@users.noreply.github.com>
2025-01-22 23:35:00 -0330

    states made

---
+++ src/MovieTheater.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+
+
+public class MovieTheater {
+
+    private final int rows;
+    private final int seatsPerRow;
+    private final char[][] seats;
+
+
+
+
+
+
+
+
+
+}
